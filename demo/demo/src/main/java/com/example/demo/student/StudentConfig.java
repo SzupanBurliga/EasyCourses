bb882d03378f1ea4aab5aa61fe11d9e99387f936
SzupanBurliga <szburliga@gmail.com>
2024-08-11 20:48:03 +0200

    1 kurs skonczony xd

--- demo/demo/src/main/java/com/example/demo/student/StudentConfig.java
+++ demo/demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Configuration
 public class StudentConfig {
@@ -14,8 +15,15 @@ public class StudentConfig {
             Student mariam = new Student(
                     "Mariam",
                     "mariam.jamal@gmail.com",
-                    LocalDate.of(2000, 1, 5),
-                    21
+                    LocalDate.of(2000, 1, 5)
+            );
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, 1, 5)
+            );
+            repository.saveAll(
+                    List.of(mariam, alex)
             );
         };
     }
