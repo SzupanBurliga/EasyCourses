57af8ddd0501d01389dcba41f1fb8c2a41095262
SzupanBurliga <szburliga@gmail.com>
2024-08-12 23:18:05 +0200

    niezly freestyle bedzie

--- React/react-app/src/App.tsx
+++ React/react-app/src/App.tsx
@@ -1,7 +1,14 @@
 
 import React from 'react';
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 function App(){
-    return <div><Message /></div>
+    let items = ["New York", "Budzow","Skawica","Makow Podhalanski","Zakopane"];
+    const handleSelectItem = (item: string) => {
+        console.log(item);
+
+    }
+    return (
+        <div><ListGroup items ={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
+    );
 }
 export default App;
